f4d8380e406981948125baf4792bc79747e02d3d
David Weston Webb <westwebb@gmail.com>
2019-01-10 13:26:34 -0500

    Create string_functions.php

---
+++ string_functions.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+
+<?php
+
+$string = "Hello";
+echo strlen($string);
+// numbers how many characters are specified
+
+echo "<br>";
+
+echo strtoupper($string);
+// makes all specified characters uppercase
+
+echo "<br>";
+
+echo strtolower($string);
+// makes all specified characters lowercase
+
+echo "<br>";
+
+
+?>
+
+</body>
+</html>
